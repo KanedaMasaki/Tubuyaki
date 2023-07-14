b36caa9bae0f4f337b2d49a4a92cf081bc4510aa
masaki-k <masaki-k@es4.eedept.kobe-u.ac.jp>
2023-07-14 11:59:11 +0900

    first commit

--- src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/service/TsubuyakiException.java
+++
@@ -1,8 +0,0 @@
-package jp.kobe_u.cs.daikibo.tsubuyaki.service;
-
-public class TsubuyakiException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public TsubuyakiException(String message) {
-        super(message);
-    }
-}
